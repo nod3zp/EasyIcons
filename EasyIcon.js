5f8655d372d9ed5b6ff2557ce29ca118195bf7f1
True Nod3r <66841380+nod3zp@users.noreply.github.com>
2020-08-23 11:05:29 +0300

    Add files via upload

---
+++ EasyIcon.js
@@ -0,0 +1,19 @@
+/*
+    Module for Insert Icons in Header shop on Moscow OVH
+    Author: nod3zp
+    Special for https://darkplugins.ru/
+*/
+
+class EasyIcons {
+    static constructor(iconsArray)
+    {
+        iconsArray.forEach((icon)=>{
+            document.querySelectorAll(icon.tag).forEach((currentObject)=>{
+                if(currentObject.innerHTML == icon.where)
+                {
+                    currentObject.innerHTML = `<i class='${icon.icon}'></i>`;
+                }
+            });
+        });
+    }
+}
